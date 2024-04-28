20db9c3cae72d551fc8f7248a28e13c9af7ad2a1
yOHO <121672362+yanhanruan@users.noreply.github.com>
2024-04-28 22:16:43 +0800

    github test

--- LESSON05/src/copyright.ts
+++ LESSON05/src/copyright.ts
@@ -5,18 +5,18 @@
 // year.textContent = thisYear;
 
 // 1st variation:
-// let year: HTMLElement | null
-// year = document.getElementById("year");
-// let thisYear: string
-// thisYear = new Date().getFullYear().toString();
-// if (year) {
-//     year.setAttribute("datetime", thisYear);
-//     year.textContent = thisYear;
-// }
+let year: HTMLElement | null
+year = document.getElementById("year");
+let thisYear: string
+thisYear = new Date().getFullYear().toString();
+if (year) {
+    year.setAttribute("datetime", thisYear);
+    year.textContent = thisYear;
+}
 
 
 // 2nd variation:
-const year = document.getElementById("year") as HTMLSpanElement
-const thisYear = new Date().getFullYear().toString()
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
+// const year = document.getElementById("year") as HTMLSpanElement
+// const thisYear = new Date().getFullYear().toString()
+// year.setAttribute("datetime", thisYear);
+// year.textContent = thisYear;
